4308857e9f7346bb01c60993725b64ff04a4d30e
Szabó Csaba <lessercodes@gmail.com>
2022-11-11 18:31:17 +0000

    Initialize project

---
+++ src/test/java/com/lessercodes/springsecuritybasics/SpringSecurityBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lessercodes.springsecuritybasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
