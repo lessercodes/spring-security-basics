534d2e788a6d6e480bc7ad7117cc1d6f26e40863
lessercodes <lessercodes@gmail.com>
2022-11-14 22:09:00 +0100

    Configure UserDetailsManager to provide a custom user

--- src/main/java/com/lessercodes/springsecuritybasics/config/WebSecurityConfig.java
+++ src/main/java/com/lessercodes/springsecuritybasics/config/WebSecurityConfig.java
@@ -1,9 +1,13 @@
 package com.lessercodes.springsecuritybasics.config;
 
+import lombok.val;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,4 +26,14 @@ public class WebSecurityConfig {
                 .build();
     }
 
+    @Bean
+    public UserDetailsManager userDetailsManager() {
+        val john = User.builder()
+                .username("john")
+                .password("password")
+                .roles("STUDENT")
+                .build();
+
+        return new InMemoryUserDetailsManager(john);
+    }
 }
