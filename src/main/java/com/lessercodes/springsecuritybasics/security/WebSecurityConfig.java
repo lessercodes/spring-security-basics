4ad8684cdee6e7846b48ce13c8608703162badfb
lessercodes <lessercodes@gmail.com>
2022-11-19 20:03:18 +0100

    Use method security to secure the Student management API

--- src/main/java/com/lessercodes/springsecuritybasics/security/WebSecurityConfig.java
+++ src/main/java/com/lessercodes/springsecuritybasics/security/WebSecurityConfig.java
@@ -3,7 +3,7 @@ package com.lessercodes.springsecuritybasics.security;
 import lombok.val;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -13,13 +13,15 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static com.lessercodes.springsecuritybasics.security.Permissions.COURSE_WRITE;
 import static com.lessercodes.springsecuritybasics.security.Roles.ADMIN;
 import static com.lessercodes.springsecuritybasics.security.Roles.ADMIN_TRAINEE;
 import static com.lessercodes.springsecuritybasics.security.Roles.STUDENT;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true
+)
 public class WebSecurityConfig {
 
     @Bean
@@ -28,10 +30,6 @@ public class WebSecurityConfig {
                 .authorizeRequests(conf -> {
                     conf.antMatchers("/", "index", "/css/**", "/js/**");
                     conf.antMatchers("/api/**").hasRole(STUDENT.name());
-                    conf.antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission());
-                    conf.antMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission());
-                    conf.antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission());
-                    conf.antMatchers(HttpMethod.GET, "/management/api/**").hasAnyRole(ADMIN.name(), ADMIN_TRAINEE.name());
                     conf.anyRequest().authenticated();
                 })
                 .httpBasic()
--- src/main/java/com/lessercodes/springsecuritybasics/student/StudentManagementController.java
+++ src/main/java/com/lessercodes/springsecuritybasics/student/StudentManagementController.java
@@ -1,6 +1,7 @@
 package com.lessercodes.springsecuritybasics.student;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,21 +21,26 @@ public class StudentManagementController {
     private final StudentService studentService;
 
     @GetMapping
+    @PreAuthorize("hasAnyRole(T(com.lessercodes.springsecuritybasics.security.Roles).ADMIN.name(), " +
+            "T(com.lessercodes.springsecuritybasics.security.Roles).ADMIN_TRAINEE.name())")
     public List<Student> getAllStudents() {
         return studentService.getAllStudents();
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority(T(com.lessercodes.springsecuritybasics.security.Permissions).COURSE_WRITE.getPermission())")
     public void registerStudent(@RequestBody Student student) {
         studentService.registerStudent(student);
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority(T(com.lessercodes.springsecuritybasics.security.Permissions).COURSE_WRITE.getPermission())")
     public void deleteStudent(@PathVariable Long id) {
         studentService.deleteStudent(id);
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasAuthority(T(com.lessercodes.springsecuritybasics.security.Permissions).COURSE_WRITE.getPermission())")
     public void updateStudent(@PathVariable Long id, @RequestBody Student student) {
         studentService.updateStudent(id, student);
     }
